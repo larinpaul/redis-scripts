24905d9d797da0f7a97abb071ff3e9c68646fd79
Pavel Larin <www.larinpavel@mail.ru>
2024-07-14 19:04:32 +0500

    Function before being declared with Async

--- asynch_await_in_java_script.js
+++ asynch_await_in_java_script.js
@@ -5,5 +5,35 @@
 //               Async doesn't have resolve or reject parameters
 //               Everything after Await is placed in an event queue.
 
+function walkDog() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const dogWalked = true;
+
+            if (dogWalked) {
+                resolve("You walk the dog :)");
+            }
+            else {
+                reject("You DIDN'T walk the dog...");
+            }
+        }, 1500);
+    });
+}
+
+function cleanKitchen() {
+
+}
+
+function takeOutTrash() {
+
+}
+
+walkDog.dog().then(value => {console.log(value); return cleanKitchen()})
+             .then(value => {console.log(value); return takeOutTrash()})
+             .then(value => {console.log(value); console.log("You finished all the chores")})
+             .catch(error => console.error(error));
+
+console.log("Hey there! :)");
 
 
